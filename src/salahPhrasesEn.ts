0db207fa3577b21fafe72775aba2a8030a364096
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2026-08-27 20:43:32 +0300

    update phrases

--- src/salahPhrasesEn.ts
+++ src/salahPhrasesEn.ts
@@ -1,8 +1,6 @@
 export const salahPhrasesEn: string[] = [
-  "O Allah, send blessings and peace upon our Master Muhammad and his family as many times as the perfection of Allah.",
-  "O Allah, bless our Master Muhammad, the healer of hearts and their medicine, the health of bodies and their cure.",
-  "O Allah, bless our Master Muhammad, a prayer by which You open for us the doors of goodness and ease.",
-  "O Allah, bless our Master Muhammad, a prayer by which You protect us from all harm and trials.",
-  "O Allah, send blessings upon Muhammad and the family of Muhammad, as You sent blessings upon Abraham.",
-  "Peace and blessings be upon our Prophet Muhammad ﷺ",
+  "O Allah, bless Muhammad & his family as You blessed Ibrahim and his family",
+  "O Allah, bless Muhammad & his family with a blessing matching Your perfection",
+  "O Allah, bless Muhammad & his family by the count of those who remember and those who forget.",
+  "O Allah, bless Muhammad, the medicine for hearts and health for bodies",
 ];
