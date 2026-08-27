403eac52ab2229626f2211eab99b62991a66bd76
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2026-08-27 20:15:33 +0300

    Update phrases

--- src/salahPhrasesEn.ts
+++ src/salahPhrasesEn.ts
@@ -3,4 +3,4 @@ export const salahPhrasesEn: string[] = [
   "O Allah, bless Muhammad & his family with a blessing matching Your perfection",
   "O Allah, bless Muhammad & his family by the count of those who remember and those who forget.",
   "O Allah, bless Muhammad, the medicine for hearts and health for bodies",
-];
+ ];
